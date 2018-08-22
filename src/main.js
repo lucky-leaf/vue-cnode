4cd66e45afe5cc87170ffe3c92c97d9285f729d3
shunkie <shunkie@qq.com>
2018-08-23 00:21:52 +0800

    use element-ui instead of bulma

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App'
-import 'bulma/css/bulma.css'
 import store from './store'
 import router from './router'
 
