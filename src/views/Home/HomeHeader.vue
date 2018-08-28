f212bc96ce9e5072179f651f7dfaaf159d0cc03f
shunkie <shunkie@qq.com>
2018-08-28 23:41:33 +0800

    remove HomeHeader style

--- src/views/Home/HomeHeader.vue
+++ src/views/Home/HomeHeader.vue
@@ -24,32 +24,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .navbar {
-    display: flex;
-    align-items: center;
-    color: #303133;
-    font-size: 14px;
-    height: 60px;
-    margin-bottom: 5px;
-    box-shadow: 0 2px 2px #f5f5f5;
-
-    .email {
-      margin-left: 10px;
-      margin-right: auto;
-    }
-
-    ul.navbar-menu {
-      display: flex;
-      margin: 0;
-      padding: 0;
-      list-style: none;
-
-      .navbar-item {
-        padding: 10px 15px;
-        &:last-child {
-          margin-right: 10px;
-        }
-      }
-    }
-  }
 </style>
