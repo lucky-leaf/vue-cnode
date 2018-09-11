b81b552689999c46fbac2a846417601ab71470b7
shunkie <shunkie@qq.com>
2018-09-11 23:39:58 +0800

    reset ListItem

--- src/views/Home/components/ListItem.vue
+++ src/views/Home/components/ListItem.vue
@@ -4,36 +4,7 @@
 
 <script>
 export default {
-  name: 'ListItem',
-  props: {
-    item: {
-      type: Object,
-      default: () => {
-        return {
-          title: ''
-        }
-      }
-    }
-  },
-  data () {
-    return {
-      types: {
-        dev: '测试',
-        share: '分享',
-        ask: '问答',
-        job: '招聘',
-        good: '精华'
-      }
-    }
-  },
-  computed: {
-    type () {
-      return this.types[this.item.tab]
-    },
-    to () {
-      return '/topic/' + this.item.id
-    }
-  }
+  name: 'ListItem'
 }
 </script>
 
