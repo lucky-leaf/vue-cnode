82cd5fd75e7150a016f9294fd43df10bd955e800
shunkie <shunkie@qq.com>
2018-09-17 02:22:37 +0800

    change actions style

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,25 +4,18 @@ import {
 
 export default {
   async GET_TOPICS ({ state, commit }, { page, tab, limit, mdrender }) {
-    commit('SET_TAB', {
-      tab
-    })
+    commit('SET_TAB', { tab })
     const { data } = await getTopics({
       page, tab, limit, mdrender
     })
-    commit('SET_LIST', {
-      tab,
-      data: data.data
-    })
+    commit('SET_LIST', { tab, data: data.data })
   },
 
   async GET_TOPIC ({ state, commit }, { id, mdrender, accesstoken }) {
     const { data } = await getTopicById({
       id, mdrender, accesstoken
     })
-    commit('SET_TOPIC', {
-      data: data.data
-    })
+    commit('SET_TOPIC', { data: data.data })
   },
 
   async checkAccessToken ({ state, commit }, accesstoken) {
