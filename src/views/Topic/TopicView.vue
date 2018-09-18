1937e79b24460ee8c379555384675a5dd89e4bef
shunkie <shunkie@qq.com>
2018-09-19 00:05:23 +0800

    make the topic view right

--- src/views/Topic/TopicView.vue
+++ src/views/Topic/TopicView.vue
@@ -7,13 +7,43 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 import TopicHeader from './TopicHeader'
 import TopicContent from './TopicContent'
 import TopicReply from './TopicReply'
 
 export default {
   name: 'TopicView',
-  components: { TopicHeader, TopicContent, TopicReply }
+  components: { TopicHeader, TopicContent, TopicReply },
+  props: {
+    id: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    ...mapState(['topic', 'mdrender', 'accessToken'])
+  },
+  created () {
+    this.GET_TOPIC({
+      id: this.id,
+      mdrender: this.mdrender,
+      accesstoken: this.accessToken
+    })
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.GET_TOPIC({
+      id: to.params.id,
+      mdrender: this.mdrender,
+      accesstoken: this.accessToken
+    }).then(() => {
+      next()
+    })
+  },
+  methods: {
+    ...mapActions(['GET_TOPIC'])
+  }
 }
 </script>
 
