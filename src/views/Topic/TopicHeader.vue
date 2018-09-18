2263da9407615d473fc38c72e5035fe449891c93
shunkie <shunkie@qq.com>
2018-09-19 03:40:28 +0800

    add el-tag

--- src/views/Topic/TopicHeader.vue
+++ src/views/Topic/TopicHeader.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>Header</div>
+  <div class="topic-header">
+    <el-tag
+      v-if="topic.top"
+      :hit="true"
+      size="small"
+      type="danger">置顶</el-tag>
+    <el-tag
+      v-if="topic.good"
+      :hit="true"
+      size="small"
+      type="warning">精华</el-tag>
+    <el-tag
+      v-if="topic.tab"
+      :hit="true"
+      size="small"
+      type="success">{{ topic.tab }}</el-tag>
+    <h1 class="title">{{ topic.title }}</h1>
+  </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  name: 'TopicHeader',
+  computed: {
+    ...mapState(['topic'])
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .topic-header {
+    .title {
+      font-size: 22px;
+      margin: 10px 0;
+    }
+  }
 </style>
