ac870652456c85391c25ffa2b623255dd066560d
shunkie <shunkie@qq.com>
2018-09-20 11:25:03 +0800

    change topic-header style and logic

--- src/views/Topic/TopicHeader.vue
+++ src/views/Topic/TopicHeader.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="topic-header">
-    <el-tag
-      v-if="topic.top"
-      :hit="true"
-      size="small"
-      type="danger">置顶</el-tag>
-    <el-tag
-      v-if="topic.good"
-      :hit="true"
-      size="small"
-      type="warning">精华</el-tag>
-    <el-tag
-      v-if="topic.tab"
-      :hit="true"
-      size="small"
-      type="success">{{ tab }}</el-tag>
-    <h1 class="title">{{ topic.title }}</h1>
+    <div class="full-title">
+      <el-tag
+        v-if="topic.top"
+        :hit="true"
+        size="small"
+        type="danger">置顶</el-tag>
+      <el-tag
+        v-if="topic.good"
+        :hit="true"
+        size="small"
+        type="warning">精华</el-tag>
+      <el-tag
+        v-if="topic.tab"
+        :hit="true"
+        size="small"
+        type="success">{{ tab }}</el-tag>
+      <h1 class="title">{{ topic.title }}</h1>
+    </div>
+    <div class="info">
+      <span> 发布于 {{ relativeTime }}</span>
+      <span> 作者 {{ topic.author.loginname }}</span>
+      <span> {{ topic.visit_count }} 次浏览</span>
+      <el-button
+        type="success"
+        size="small">收藏</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import moment from 'moment'
 import types from '../../mixins/types'
 
 export default {
@@ -30,6 +41,9 @@ export default {
     ...mapState(['topic']),
     tab () {
       return this.types[this.topic.tab]
+    },
+    relativeTime () {
+      return moment(this.topic.create_at).fromNow()
     }
   }
 }
@@ -37,18 +51,38 @@ export default {
 
 <style lang="scss" scoped>
   .topic-header {
-    display: flex;
-    align-items: center;
+    .full-title {
+      display: flex;
+      align-items: center;
 
-    .el-tag {
-      & + .el-tag {
-        margin-left: 10px;
+      .el-tag {
+        & + .el-tag {
+          margin-left: 10px;
+        }
+      }
+
+      .title {
+        font-size: 22px;
+        margin: 0 0 0 10px;
       }
     }
 
-    .title {
-      font-size: 22px;
-      margin: 0 0 0 10px;
+    .info {
+      display: flex;
+      align-items: center;
+      margin-top: 10px;
+      font-size: 12px;
+      color: #757575;
+
+      span {
+        &::before {
+          content: "•";
+        }
+      }
+
+      .el-button {
+        margin-left: auto;
+      }
     }
   }
 </style>
