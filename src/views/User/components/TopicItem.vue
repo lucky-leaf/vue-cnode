9f8cf615683e4e73c6ec87875134da8750305e33
shunkie <shunkie@qq.com>
2018-09-23 23:39:41 +0800

    add components list and item

---
+++ src/views/User/components/TopicItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="topic-item">
+    TopicItem
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopicItem'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/User/components/TopicList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="topic-list">
+    TopicList
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopicList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
