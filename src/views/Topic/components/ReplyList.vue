7d63192efa75c62bae2a0038d41a781802dda74e
shunkie <shunkie@qq.com>
2018-09-26 05:57:19 +0800

    add component reply-list

---
+++ src/views/Topic/components/ReplyList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="reply-list">
+    ReplyList
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReplyList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
