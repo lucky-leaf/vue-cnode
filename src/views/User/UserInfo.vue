1ccc973a8d38aed7e8ac918bcaa7dc7d7a8d318f
shunkie <shunkie@qq.com>
2018-09-25 23:13:57 +0800

    add user profile

--- src/views/User/UserInfo.vue
+++ src/views/User/UserInfo.vue
@@ -8,13 +8,37 @@
         class="title">
         用户信息
       </div>
+      <div class="user-profile">
+        <img
+          :src="user.avatar_url"
+          class="avatar"
+          alt="avatar">
+        <span class="score">{{ user.score }}积分</span>
+        <a
+          :href="githubUrl"
+          target="_blank">@{{ user.githubUsername }}</a>
+        <span class="created-time">注册时间{{ relativeTime }}</span>
+      </div>
     </el-card>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import moment from 'moment'
+
 export default {
-  name: 'UserInfo'
+  name: 'UserInfo',
+  computed: {
+    ...mapState(['user']),
+    relativeTime () {
+      return moment(this.user.create_at).fromNow()
+    },
+    githubUrl () {
+      return `https://github.com/${this.user.githubUsername}`
+    }
+  }
+
 }
 </script>
 
@@ -27,9 +51,15 @@ export default {
         margin: -18px -20px;
         padding: 18px 20px;
         border-left: 5px solid #50bfff;
-        background-color: #ecf8ff;
         font-size: 16px;
         font-weight: 700;
+      }
+
+      .user-profile {
+        .avatar {
+          width: 120px;
+          height: 120px;
+        }
       }
     }
   }
