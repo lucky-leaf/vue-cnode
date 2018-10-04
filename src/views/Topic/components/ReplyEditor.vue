472e3f2fac5745166ae2999bdeb982510504c62c
shunkie <shunkie@qq.com>
2018-10-04 22:57:34 +0800

    reply editor add title css

--- src/views/Topic/components/ReplyEditor.vue
+++ src/views/Topic/components/ReplyEditor.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="reply-editor">
-    ReplyEditor
+    <div class="title">
+      添加回复
+    </div>
   </div>
 </template>
 
@@ -11,5 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .reply-editor {
+    margin: -20px;
 
+    .title {
+      padding: 10px;
+      font-size: 18px;
+      line-height: 24px;
+      border-bottom: 1px solid #f0f0f0;
+    }
+  }
 </style>
